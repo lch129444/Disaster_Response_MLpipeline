ff6ab9dfda6946b368ba47595b05e53e122396b1
Charline <lch129444@hotmail.com>
2022-09-05 18:25:15 -0500

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,19 +1,95 @@
-import sys
+"""
+Preprocessing of Data
+Project: Disaster Response Pipeline 
+**Udacity - Data Science Nanodegree**
 
+Sample Script Syntax:
+
+python process_data.py <path to messages csv file> <path to categories csv file> <path to sqllite  destination db>
+
+Sample Script Execution:
+python process_data.py disaster_messages.csv disaster_categories.csv disaster_response_db.db
+
+Arguments Description:
+    1) Path to the messages CSV file(e.g. disaster_messages.csv)
+    2) Path to the categories CSV file(e.g. disaster_categories.csv)
+    3) Path to SQLite destination database (e.g. disaster_response_db.db)
+"""
+
+import sys
+import numpy as np
+import pandas as pd
+import os
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+     """
+    Load Messages Data, the Categories Data, and merge the 2
+    
+    Arguments:
+        messages_filepath: Path to the messages CSV file
+        categories_filepath: Path to the categories CSV file
+    Output:
+        df: Merged data containing messages and categories
+    """
+    
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages,categories,on='id')
+    return df 
 
 
 def clean_data(df):
-    pass
+    """
+    Clean the above Dataframe with category names and binarlized values
+    
+    Arguments:
+        df: Merged data with messages and categories
+    Outputs:
+        df: above datafarme with categories cleaned up, values binalized
+    """
+    
+    # Split the categories
+    categories = df['categories'].str.split(pat=';',expand=True)
+    
+    #Fix the categories columns name
+    row = categories.iloc[[0]]
+    category_colnames = list(row.apply(lambda category_name: category_name.str.split('-')[0][0]))
+    categories.columns = category_colnames
+    
+    for column in categories:
+        categories[column] = categories[column].astype(str).str[-1:]
+        categories[column] = categories[column].astype(int)
+    
+    df = df.drop(['categories'], axis=1, inplace=True)
+    df = pd.concat([df,categories], join='inner', axis=1)
+    df = df.drop_duplicates(inplace=True)
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save Data to SQLite Database Function
+    
+    Arguments:
+        df: Combined data containing messages and categories with categories cleaned up
+        database_filename: Path to SQLite destination database
+    """
+    
+    engine = create_engine('sqlite:///'+ database_filename)
+    table_name = database_filename.replace(".db","") + "_table"
+    df.to_sql(table_name, engine, index=False, if_exists='replace')  
 
 
 def main():
+        """
+    The Main to start the data processing functions. 
+    There are three steps:
+        1) Load Messages Data and Categories Data
+        2) Clean Categories Data
+        3) Save Data to SQLite Database
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
